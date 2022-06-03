2cbb689f5dda3f81376e3a5a217c186b0b1e8778
David D. Riddle <ddriddle@illinois.edu>
2022-06-03 17:01:00 -0500

    Add VCR test for failure

--- tests/test_connector.py
+++ tests/test_connector.py
@@ -1,10 +1,13 @@
 import json
 import os
 import sys
+from urllib.error import HTTPError
 
 import pytest
 
 from unittest.mock import patch, Mock
+import vcr
+from requests.exceptions import HTTPError
 
 from phsoar_null_router.soar_null_router_connector import Soar_Null_RouterConnector
 from bhr_client.rest import Client as BHRClient
@@ -29,7 +32,7 @@ def test_connectivity(mock, connector: Soar_Null_RouterConnector):
 
 @patch("phsoar_null_router.soar_null_router_connector.login_from_env")
 def test_block(mock, connector: Soar_Null_RouterConnector):
-    cidr = '157.55.39.89/32'
+    cidr='192.0.2.1/32'
 
     mock.return_value = Mock(spec=BHRClient)
     in_json = {
@@ -48,6 +51,19 @@ def test_block(mock, connector: Soar_Null_RouterConnector):
     # Assertion
     assert action_result[0]["message"] == f"Blocked {cidr}"
     mock.return_value.block.assert_called_once_with(
-        cidr='157.55.39.89/32', 
+        cidr='192.0.2.1/32',
         source='SOAR', 
         why='Appears in our suspicious event list.')
+
+@vcr.use_cassette()
+def test_connectivity_fail(connector: Soar_Null_RouterConnector):
+    in_json = {
+            "appid": "fceeaac1-8f96-46d6-9c3b-896e363eb004",
+            "parameters": {
+                "identifier": "test_connectivity",
+            }
+    }
+
+    # Execute Action
+    with pytest.raises(HTTPError):
+        action_result_str = connector._handle_action(json.dumps(in_json), None)
