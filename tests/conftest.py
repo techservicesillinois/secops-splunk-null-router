965215d0c515988a5faab7975ca70f1c597108ec
David D. Riddle <ddriddle@illinois.edu>
2023-06-29 14:21:46 -0500

    Add play_count test
    Closes #40

--- tests/conftest.py
+++ tests/conftest.py
@@ -58,3 +58,4 @@ def cassette(request) -> vcr.cassette.Cassette:
         if my_vcr.record_mode == 'none':  # Tests only valid when not recording
             assert tape.all_played, \
                 f"Only played back {len(tape.responses)} responses"
+            assert tape.play_count == 1
